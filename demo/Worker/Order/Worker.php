b5eb8e4e04bd7fec24f61682e532b6dd34768a2e
Nadhiir <nadhiir@ipedis.com>
2020-04-19 03:57:46 +0400

    order worker : change architecture to accept multiple binding keys

--- demo/Worker/Order/Worker.php
+++ demo/Worker/Order/Worker.php
@@ -5,7 +5,6 @@ namespace Ipedis\Demo\Rabbit\Worker\Order;
 
 use AMQPEnvelope;
 use Ipedis\Demo\Rabbit\Utils\ConnectorAbstract;
-use Ipedis\Rabbit\Channel\OrderChannel;
 use Ipedis\Rabbit\Consumer\Handler\MessageHandlerInterface;
 use Ipedis\Rabbit\MessagePayload\OrderMessagePayload;
 use Ipedis\Rabbit\MessagePayload\ReplyMessagePayload;
@@ -16,10 +15,11 @@ class Worker extends ConnectorAbstract
 {
     use WorkerTrait;
 
-
-    public static function getQueueName(): string
+    protected function getBindingKey()
     {
-        return OrderChannel::fromString('v1.admin.publication.generate');
+        return [
+            'v1.admin.publication.generate',
+        ];
     }
 
     protected function makeMessageHandler(): \Closure
--- src/Order/Worker.php
+++ src/Order/Worker.php
@@ -191,11 +191,29 @@ trait Worker
     protected function queueDeclare()
     {
         $this->queue = new AMQPQueue($this->channel);
-        $this->queue->setName($this->getQueueName());
         $this->queue->setFlags(AMQP_DURABLE);
         $this->queue->declareQueue();
+        $this->resolveRoutingKeys();
+    }
 
-        $this->queue->bind($this->getExchangeName(), $this->getQueueName());
+    /**
+     * @throws \AMQPChannelException
+     * @throws \AMQPConnectionException
+     */
+    private function resolveRoutingKeys()
+    {
+        $routingKey = $this->getBindingKey();
+        // If is string, we cast it to array.
+        if(is_string($routingKey)) $routingKey = [$routingKey];
+
+        if(is_array($routingKey)) {
+            foreach ($routingKey as $key) {
+                if(is_string($key)) {
+                    $this->queue->bind($this->exchange->getName(), $key);
+                }
+            }
+        }
+
     }
 
     /**
@@ -208,11 +226,11 @@ trait Worker
     }
 
     /**
-     * The queue name to be used by the worker
+     * Can be string or array of keys
      *
-     * @return string
+     * @return mixed
      */
-    abstract public function getQueueName(): string;
+    abstract protected function getBindingKey();
 
     /**
      * The exchange to be used to bind worker's queue
