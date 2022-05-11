4e2425e5e890f49309ee5c4dcbf4176a4177a933
Ruqayyah Jaumeer <ruqayyah@ipedis.com>
2022-05-11 09:52:00 +0400

    checking

--- tests/MessagePayload/EventPayloadAbstractTest.php
+++ tests/MessagePayload/EventPayloadAbstractTest.php
@@ -84,6 +84,11 @@ it('uuid can be defined on header parameter', function () use ($channelName) {
     $this->assertEquals($uuid, $event->getUuid());
 });
 
+it('contain sendAt timezone by default', function () use ($channelName) {
+    $event = EventMessagePayload::build($channelName);
+    $this->assertIsInt($event->getTimestamp());
+});
+
 it('should return data encoded in json', function () use ($channelName) {
     $event = EventMessagePayload::build($channelName, ['some' => 'data']);
     $this->assertJsonStringEqualsJsonString(json_encode(['some' => 'data']), $event->getStringifyData());
